2351f539ec10510da4f1469b37bb0f6860effc7b
cao-shd <cao-shd@outlook.com>
2020-05-30 23:04:19 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 创建DOM JSX语法
+const div = <div id="myDiv" title="this is a div">这是一个div元素</div>;
+// 渲染DOM
+ReactDOM.render(div, document.getElementById('app'));
+
+
